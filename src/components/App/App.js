eb8c75b2c3e63c4c7faa02d604efbddccfe56bed
Leigh Steiner <steinerleigh@gmail.com>
2018-04-01 17:52:33 -0400

    headline grow animation added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
-// import logo from './logo.svg';
 import './style.css';
 import BookList from '../BookList.js';
 
